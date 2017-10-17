f796fb4c68111729b76317a80659330104b654ce
cloudZQY <498219667@qq.com>
2017-10-17 23:45:55 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function getData() {
+  return new Promise(function(resolve) {
+    $.get('/db/manka.json', function(data) {
+      resolve(data);
+    });
+  })
+}
+function getMankaTemp() {
+  return new Promise(function(resolve) {
+    $.get('/temple/mankas.html', function(data) {
+      resolve(data);
+    })
+  })
+}
+Promise.all([getData(), getMankaTemp()]).then(function(res) {
+  var data = res[0];
+  var temp = res[1];
+  var html = doT.template(temp)(data);
+  var container = document.querySelector('.manka_container');
+  container.innerHTML = html;
+})
---
+++ server.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var debug = require('debug')('expdemo:server');
+var http = require('http');
+var express = require('express');
+
+var app = express();
+
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname)));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+  console.log('Listening on ' + bind);
+}
+
